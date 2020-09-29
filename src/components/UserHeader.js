48d6f758a5703e69d4916c3f0ded7c85319a7ef2
anupamroy8 <anupamroy8@gmail.com>
2020-09-29 22:52:01 +0530

    memoize added to fetchUser action creator

---
+++ src/components/UserHeader.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
+    }
+    return (
+      <div>
+        <p className="header">{user.name}</p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownprops) => {
+  return {
+    user: state.users.find((user) => user.id === ownprops.userId),
+  };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
